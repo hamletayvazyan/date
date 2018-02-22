fa2568d71d07870e3d80eaba6a734f60c83002a4
Hamlet <hamlet94@live.ru>
2018-02-22 19:47:18 +0400

    Update FileJS.js

--- FileJS.js
+++ FileJS.js
@@ -1,84 +1,85 @@
 var start = $('.General').val();
-var div = $('#matrix').val();
 var months = $('#Months');
 var matrix = $('#matrix');
+var matr = $('#matr');
 var masiv = [];
-var masivOne;
 var namesMonth = [];
 var weekName = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
-$(window).on('load', function () {
-
-    $.ajax({
-        type: "GET",
-        url: "datePhP.php",
-        data: {
-            load: start,
-            divs: div
-        }
-    }).done(function (response) {
-
-        masiv = response;
-        var asd = masiv.split(',');
-        console.log(asd);
-
-        for (var h = 0; h < 7; h++){
-            matrix.append('<div class="week">'+weekName[h]+'</div>');
-        }
-       /*
-        var Month = asd[0];
-        var bone = asd[4];
-        var Days = parseInt(bone) + 1;
-        months.append(Month);
-
-        var masiv1 = namesMonth.split(' ');
-        var res = masiv1.slice(0, 12);
-        var correctMonth = res.indexOf(Month);
-        console.log(correctMonth);
-        */
-      /*  matri();
-        function matri() {
-            var count = 1;
-            for (var i = 0; i < 5; i++){
-                matrix.append('<br>');
-                for (var j = 0; j < 7; j++){
-                    if (i == 0){
-                        if (j < parseInt(posDate)){
-                            matrix.append('<div class="days">&nbsp;</div>');
-                        }else if (j >= parseInt(posDate)){
-                            matrix.append('<div class="days">'+count+'</div>');
-                            count++;
-                        }
-                    }else{
-                        matrix.append('<div class="days">'+count+'&nbsp;</div>');
-                        count++;
-
-                    }if (count === Days){
-                        break
-                    }
-
-                }
+for (var h = 0; h < 7; h++){
+    matr.append('<div class="week">'+weekName[h]+'</div>');
+}
+nextclick();
+function nextclick() {
+    var count = 0;
+    var count1 = 0;
+    $('.next').on('click', function () {
+        count += 1;
+        count1 += 1;
+        $.ajax({
+            type: 'GET',
+            url: 'datePhP.php',
+            data: {
+                next: count
             }
-            for (var d = 0; d < posDate1; d++){
-                matrix.append('<div class="days">&nbsp;</div>');
-            }
-        }
-*/
+        }).done(function (event) {
+            masiv = [];
+            masiv = event;
+            var asd = masiv.split(',');
+            console.log(asd);
+            matrix.html(asd[4]);
+            namesMonth = asd[1];
+            var masiv1 = namesMonth.split(' ');
+            var res = masiv1.slice(0, 12);
+            var Month = asd[3];
+            var correctMonth = res.indexOf(Month) + count1;
+            console.log(correctMonth);
+            months.html(res[correctMonth])
+
+        })
     });
 }
+prevclick();
+
+function prevclick() {
+    var count = 0;
+    var count1 = 0;
+    $('.prev').on('click', function () {
+        count += 1;
+        count1 += 1;
+        $.ajax({
+            type: 'GET',
+            url: 'datePhP.php',
+            data: {
+                prev: count
+            }
+        }).done(function (event) {
+            masiv = [];
+            masiv = event;
+            var asd = masiv.split(',');
+            console.log(asd);
+            matrix.html(asd[5]);
+            namesMonth = asd[1];
+            var masiv1 = namesMonth.split(' ');
+            var res = masiv1.slice(0, 12);
+            var Month = asd[3];
+            var correctMonth = res.indexOf(Month) - count1;
+            months.html(res[correctMonth])
+        })
+    });
+}
+$(window).on('load', function () {
+        $.ajax({
+            type: "GET",
+            url: "datePhP.php",
+            data: {
+                load: start
+            }
+        }).done(function (response) {
+            masiv = response;
+            var asd = masiv.split(',');
+            console.log(asd);
+            months.append(asd[0]);
+            matrix.append(asd[2]);
+        });
+    }
 );
-
-
-
-/*      names.html(asd[0]);
-        matrix.html(asd[2]);
-        namesMonth = asd[3];
-        var count = 1;
-
-
-
-        $('.prev').on('click', function (event) {
-            var result = correctMonth - count;
-            count++;
-            names.html(res[result])
-        })*/
